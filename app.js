66dea050e938976b7c33d42a3698a186fc33b1b7
Subhan De <subhande@Subhans-MacBook-Air.local>
2021-12-15 21:12:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+info_1 = {
+    first_name: "subhan",
+    last_name: "de"
+}
+
+app.get("/api", (req, res, next) => {
+    res.json(info_1);
+   });
+
+
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
