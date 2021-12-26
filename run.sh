36e5d2d236769e7efe36128c7fbd0a66931d014a
Subhan De <subhande@Subhans-MacBook-Air.local>
2021-12-26 17:39:38 +0530

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-# docker rm --force app_1
-# docker build -t app_1 .
-# docker run -d --name app_1 -p 3000:3000 app_1
-docker-compose up -d --build
+docker network create app
+
+docker rm --force app_1
+docker build -t app_1 .
+docker run -d --net app --name app_1 -p 3000:3000 -t app_1 
+# docker-compose up -d --build
