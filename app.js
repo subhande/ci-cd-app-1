e778891eb4643c460df4ca6b24b5d5aabc6707bd
Subhan De <subhande@Subhans-MacBook-Air.local>
2021-12-26 16:34:22 +0530

    initial

--- app.js
+++ app.js
@@ -5,30 +5,30 @@ const axios = require('axios');
 info_1 = {
     first_name: "subhan",
     last_name: "de",
-    branch: "development",
+    branch: "developmet",
     app: "app-1"
 }
 
 app.get("/api", (req, res, next) => {
     let messages = [];
 
-      axios.get("http://0.0.0.0:6000/api")
+    axios.get("http://app-2:6000/api")
         .then(response => {
             // console.log()
             messages.push(info_1);
             messages.push(response.data);
-            res.json({messages});
+            res.json({ messages });
         })
         .catch(error => {
             console.log('error', error);
-            res.json({messages});
+            res.json({ messages });
         });
-    
-        
-   });
+
+
+});
 
 
 
 app.listen(3000, '0.0.0.0', () => {
- console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
