3a72bd516c44a65a522cc41fa48ceedebb0f9519
Subhan De <subhande@Subhans-MacBook-Air.local>
2021-12-26 15:52:00 +0530

    updated

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var express = require("express");
 var app = express();
+const axios = require('axios');
 
 info_1 = {
     first_name: "subhan",
     last_name: "de",
-    branch: "master"
+    branch: "master",
+    app: "app-1"
 }
 
 app.get("/api", (req, res, next) => {
-    res.json(info_1);
+    let messages = [];
+
+      axios.get("http://0.0.0.0:6000/api")
+        .then(response => {
+            // console.log()
+            messages.push(info_1);
+            messages.push(response.data);
+            res.json({messages});
+        })
+        .catch(error => {
+            console.log('error', error);
+            res.json({messages});
+        });
+    
+        
    });
 
 
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
  console.log("Server running on port 3000");
 });
